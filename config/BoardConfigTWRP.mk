a2d49a5394795e2d5bd10b0187d7372bb910078c
nebrassy <nebras30@gmail.com>
2021-09-12 17:29:19 +0200

    also set twrp cmdline for legacy A/B
    Change-Id: Ibb3d7076d6bd1518585c4f3b3bfcc71cc8e0ceae

--- config/BoardConfigTWRP.mk
+++ config/BoardConfigTWRP.mk
@@ -3,6 +3,7 @@ include vendor/twrp/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
 GENERIC_KERNEL_CMDLINE += twrpfastboot=1
+BOARD_KERNEL_CMDLINE += twrpfastboot=1
 endif
 
 
